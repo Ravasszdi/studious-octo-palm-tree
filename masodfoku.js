a24d2f3e56724b3450dce73c811ba523c2b53b3a
Ravasszdi <harasztimisi.rav@gmail.com>
2026-03-25 15:19:05 +0100

    izzééé3

--- masodfoku.js
+++ masodfoku.js
@@ -1,4 +1,5 @@
 export  function masodfoku(a,b,c) {
+    if(a===0) return `Az "a" egyenlő 0-val! és nem másodfokú`;
     let d = Math.sqrt((b*b)-(4*a*c));
     if(d<0) return {x1:"i",x2:"i"};
     return {
